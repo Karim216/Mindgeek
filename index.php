f6afc75effdd21e79303c51e9bc2c8d98e776d91
karim216 <ahmedkarim.njiawouogbetnkom@esprit.tn>
2021-06-25 16:59:39 +0100

    new commit add

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
-<?php
-
-include "view/accueil.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/9d8138c3b6.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Candal&family=Open+Sans&display=swap" rel="stylesheet">  
+    <link rel="stylesheet" href="css/style.css"/>
+    <title>Index</title>
+</head>
+<body>
+    <header>
+        <div>
+            <img src="img/logo.png" alt="logo"/>
+            <nav id="nav1">
+                <a href="view"><i class="fas fa-users"></i><span>Serveur mutualisé</span></a>
+                <a href="#"><i class="fas fa-user"></i><span>Serveur dédié</span></a>
+                <a href="#"><i class="fas fa-cloud"></i><span>Serveur "cloud"</span></a>
+                <a href="#"><i class="fas fa-envelope"></i><span>Contact</span></a>
+            </nav>
+        </div>
+        <div>
+            <h1>L'hébergement à prix libre</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit koll ropa liest.</p>
+        </div>
+        <div class="rech"><span>que recherchez-vous ?</span></div>
+    </header>
+    <footer>
+        <nav id="nav2">
+            <a href="#">Accueil</a>
+            <a href="#">Serveur mutualisé</a>
+            <a href="#">Serveur dédié</a>
+            <a href="#">Serveur "cloud"</a>
+            <a href="#">Contact</a>
+            <a href="#">Se connecter</a>
+        </nav>
+        <div id="licence">
+            <img src="https://www.innovavenir.com/sites/default/files/styles/news_list_image/public/fields/news/image/logo-3wa.png?itok=lVg4L6Ll" alt="logo_3wa"/>
+            <span>Cet exercice de <a href="#" rel="licence">3W Academy</a> est mis à disposition <a href="#" rel="licence">pour l'usage personnel des étudiants, Pas de Rediffusion - Attribution - Pas d'Utilisation Commerciale - Pas de Modification - International</a>.<br/>
+            Les autorisations au-delà du champ de cette licence peuvent être obtenues auprès de <a href="#">contact@3wa.fr</a>. Les maquettes ont été réalisées par <a href="#" rel="licence">Justine Muller</a>.</span>
+        </div>
+    </footer>
+</body>
+</html>
